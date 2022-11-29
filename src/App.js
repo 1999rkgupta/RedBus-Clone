1e01799fab5a419076e93944c86fa8801c66f79e
Manoharamaresh <manoharamaresh@gmail.com>
2022-11-29 16:48:46 +0530

    redbus footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,369 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+  integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="footerSec">
+        <article>
+          <main className="content">
+            <div>
+              <ul>
+                <li className="heading">Top Bus Routes</li>
+                <br />
+                <li>
+                  <a href="">Hyderabad to Bangalore Bus</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Bangalore to Chennai Bus</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Pune to Bangalore Bus</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Mumbai to Bangalore Bus</a>
+                </li>
+                <br />
+                <li>
+                  <a href="#">More</a>
+                </li>
+              </ul>
+            </div>
+            <div id="cityDiv">
+              <ul>
+                <li className="heading">Top Cities</li>
+                <br />
+                <li>
+                  <a href="">Hyderabad Bus Tickets</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Bangalore Bus Tickets</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Chennai Bus Tickets</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Pune Bus Tickets</a>
+                </li>
+                <br />
+                <li>
+                  <a href="#">More</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="rtc-div">
+              <ul>
+                <li className="heading">Top RTC's</li>
+                <br />
+                <li>
+                  <a href="">APSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">GSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">MSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">KSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="#">More</a>
+                </li>
+              </ul>
+            </div>
+            <div className="other-div">
+              <ul>
+                <li className="heading">Other</li>
+                <br />
+                <li>
+                  <a href="">TSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">SBSTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">RSRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">KeralaRTC</a>
+                </li>
+                <br />
+                <li>
+                  <a href="#">More</a>
+                </li>
+              </ul>
+            </div>
+            <div className="tempDiv">
+              <ul>
+                <li className="heading">Tempo Traveller in Cities</li>
+                <br />
+                <li>
+                  <a href="">Tempo traveller in Bangalore</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Tempo traveller in Chennai</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Tempo traveller in Mumbai</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Tempo traveller in Hyderabad</a>
+                </li>
+                <br />
+                <li>
+                  <a href="">Tempo traveller in Ahmedabad</a>
+                </li>
+              </ul>
+            </div>
+          </main>
+          <br />
+          <br />
+          <hr />
+
+          <footer id="footer-sec">
+            <h2>Top Operators</h2>
+            <br />
+            <span>
+              <a href="">SRS Travels | </a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Kallada Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">KPN Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Orange Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Parveen Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Rajdhani Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href=""> VRL Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Chartered Speed Bus |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Bengal Tiger |</a>
+            </span>
+            {/* &nbsp; */}
+            <br />
+            <br />
+            <span>
+              {/* &nbsp; */}
+              <a href="">SRM Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Infant Jesus |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href=""> Patel Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">JBT Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Shatabdi Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Eagle Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Kanker Roadways |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Komitla |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href=""> Sri Krishna Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Humsafar Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <br />
+            <br />
+            <span>
+              <a href="">Mahasagar Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Raj Express |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Sharma Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Shrinath Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Universal Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Verma Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Gujarat Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Madurai Radha Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <br />
+            <br />
+            <span>
+              <a href="">Patel Tours and Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Paulo Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Royal Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Amarnath Travels |</a>
+            </span>
+            &nbsp;
+            <span>
+              <a href="">Vaibhav Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Ganesh Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Jabbar Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Jain Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Manish Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <br />
+            <br />
+            <span>
+              <a href="">Pradhan Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">YBM Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Hebron Transports |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Mahalaxmi Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">MR Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Vivegam Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">VST Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Jakhar Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Kaleswari Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <br />
+            <br />
+            <span>
+              <a href="">Mahendra Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Neeta Tours and Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Yamani Travels |</a>
+            </span>
+            {/* &nbsp; */}
+            <span>
+              <a href="">Arthi Travels |</a>
+            </span>
+            {/* &nbsp; */}
+          </footer>
+          <h3>
+            <a href="https://www.redbus.in/travels/operators-directory">
+              All Operators
+            </a>
+          </h3>
+        </article>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
