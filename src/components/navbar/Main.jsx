ccd58d17189a72bf54b9df114f18de291536bdcf
AmithN68 <amithn68@gmail.com>
2022-11-30 13:00:09 +0530

    newly done

--- src/components/navbar/Main.jsx
+++ src/components/navbar/Main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import Styles from "./_navbar.module.css";
 
 const Main = () => {
   return (
-    <div>
+    <div id={Styles.mainSection}>
       <section id={Styles.sliderBlock}>
         <article>
           <div>
@@ -29,7 +29,7 @@ const Main = () => {
       </section>
 
       <div
-              class={Styles.first}
+        class={Styles.first}
         data-link="https://www.redbus.in/info/OfferTerms#FIRST"
         data-slot="1"
         data-creative="promotion-banner"
@@ -38,47 +38,47 @@ const Main = () => {
           <img src="https://st.redbus.in/Images/RHSS/2022/hero/100x100.png" />
         </div>
 
-        <div class="fl dib promotion-text-wrap">
+        <article class="fl dib promotion-text-wrap">
           <div class="tac promotion-header main-header-family gtm-offer-code">
             Save upto Rs 250* on your bus tickets.
           </div>
           <div class="tac promotion-description gtm-offer-desc">
             Book your favourite seat now.
           </div>
-        </div>
-
-              <section id={ Styles.saveSection } >
-                  <article className={Styles.article1}>
-            <ul>
-              <li>
-                <span>
-                  <span>Save upto Rs 300 on Ap and TS route</span>
-                  <span>
-                    <img src="https://st.redbus.in/images/offers/superhit_rav/1_1.png" />
-                  </span>
-                  <span>Use Code SUPERHIT</span>
-                </span>
-              </li>
-            </ul>
-          </article>
-
-          <article className={Styles.article2}>
-            <ul>
-              <li>
-                <span>
-                  <span>Save upto Rs 200 on Delhi and North routes</span>
-                  <span>
-                    <img src="https://st.redbus.in/Images/INDOFFER/RB200/274x148.png" />
-                  </span>
-                  <span>Use code RB200</span>
-                </span>
-              </li>
-            </ul>
-          </article>
-        </section>
+        </article>
       </div>
+
+      <section className={Styles.saveSection}>
+        <article className={Styles.article1}>
+          <ul>
+            <li>
+              <span>
+                <span>Save upto Rs 300 on Ap and TS route</span>
+                <span>
+                  <img src="https://st.redbus.in/images/offers/superhit_rav/1_1.png" />
+                </span>
+                <span>Use Code SUPERHIT</span>
+              </span>
+            </li>
+          </ul>
+        </article>
+
+        <article className={Styles.article2}>
+          <ul>
+            <li>
+              <span>
+                <span>Save upto Rs 200 on Delhi and North routes</span>
+                <span>
+                  <img src="https://st.redbus.in/Images/INDOFFER/RB200/274x148.png" />
+                </span>
+                <span>Use code RB200</span>
+              </span>
+            </li>
+          </ul>
+        </article>
+      </section>
     </div>
   );
-}
+};
 
-export default Main
+export default Main;
